3cd892e410fa5a6631ddcb2f0f2bec88e04e3447
gaurav <aadeezcity@gmail.com>
2021-09-21 18:53:38 +0530

    here we learnt about interpolation,property binding and use of constructor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
-import { UsersComponent } from './users/users.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    UsersComponent
   ],
   imports: [
     BrowserModule
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,9 +3,25 @@ import { Component } from "@angular/core";
 //this is called decorator and decorator always starts with @ sign. 
 @Component({
   selector:'my-profile', //this is the name of our component , it can be .app-root and [app-root]
-  template:'This is profile page',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 
 export class ProfileComponent{
+    name:string = 'Ramesh';
+    age:number = 20;
+    status:string = 'coder';
+    salary:number = 100; 
+    btnColor:string;
+    isDisabled:boolean = true;
 
+    getTotalNum(){
+        return this.age*this.salary
+    }
+    // when page loads constructor call automatically 
+    constructor(){
+        const colors =  ['red','green','pink','yellow','blue'];
+        this.btnColor = "btn "+colors[Math.floor(Math.random() * colors.length)];
+        setTimeout(() => this.isDisabled = false, 3000);
+    }
 }
