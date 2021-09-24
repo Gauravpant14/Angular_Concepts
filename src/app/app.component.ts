6070fb2bb91cda135400c9c69b7d229ca19ae962
gaurav <aadeezcity@gmail.com>
2021-09-24 12:34:26 +0530

    Pass Data from Parent to Child using Input decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-//this is called decorator and decorator always starts with @ sign. 
+//this is called decorator and decorator always starts with @ sign.
 @Component({
-  selector:'app-root', //this is the name of our component , it can be .app-root and [app-root]
-  templateUrl:'./app.component.html',
-  styleUrls:['./app.component.css']
+  selector: 'app-root', //this is the name of our component , it can be .app-root and [app-root]
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-
-export class AppComponent{
-
+export class AppComponent {
+  users = [
+    { name: 'React', age: 22 },
+    { name: 'Node', age: 23 },
+    { name: 'Vue', age: 21},
+    { name: 'Angular', age: 23 },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { UsersComponent } from './users/users.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
