c9fef58a56dd9e377e51c4ba74495fdf151c1cff
gaurav <aadeezcity@gmail.com>
2021-09-27 17:29:09 +0530

    custom pipe uses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,6 @@ import { User } from 'src/models/user';
 export class AppComponent {
   countryName!:string;
   countryNum: number = 567565445;
-  title:string = 'header'
-  show:boolean = true
-  users:Array<User> = [
-    { name: 'React', age: 22 },
-    // { name: 'Node', age: 23 },
-    // { name: 'Vue', age: 21},
-    // { name: 'Angular', age: 23 },
-  ];
-
-  //here we are getting child data
-  receivedData(e:any){
-    console.log(e);
-  }
-
   getInd(e:any){
    this.countryName = e.target.innerText;
   }
