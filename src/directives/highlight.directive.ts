8fc475c2dade067917639d2c422ebe30600a96bd
gaurav <aadeezcity@gmail.com>
2021-09-27 16:04:27 +0530

    accessing constructor element in whole class using constructor injection

--- src/directives/highlight.directive.ts
+++ src/directives/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef } from '@angular/core';
 })
 export class HighlightDirective {
   //when we use private inside cunstructor then  the element inside constuctor is available to the whole class
-  // this is also called as constructor injection... 
+  // this is also called as constructor injection....
   constructor(private el: ElementRef) { 
  
   }
