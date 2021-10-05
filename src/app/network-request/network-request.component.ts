311729116922a286ba2c489c9c7ad58d4e65152f
gaurav <aadeezcity@gmail.com>
2021-10-05 15:04:07 +0530

    Custom pipe

---
+++ src/app/network-request/network-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-network-request',
+  templateUrl: './network-request.component.html',
+  styleUrls: ['./network-request.component.css']
+})
+export class NetworkRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
